6c8d685b7ea2b09743f331bfc8cde3fa141a0f73
plusdojo2024-kusaka <kusaka-syota-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 11:29:00 +0900

    あ

--- doc/14_DB設計図/0617 table create/Onsen.sql
+++ doc/14_DB設計図/0617 table create/Onsen.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Onsen (
     hs_id int AUTO_INCREMENT PRIMARY KEY,
-    hs_name varchar(100),
+    onsen_name varchar(100),
     hs_address varchar(100),
     hs_phone varchar(100),
     hs_effect varchar(100),
